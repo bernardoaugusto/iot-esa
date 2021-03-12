510a2b00681ac2aba6ac2dd382c5faecdcc6bc2c
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-03-12 16:04:06 -0300

    feat: add ightbulb

--- App.tsx
+++ App.tsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons'; 
 
 export default function App() {
+  const [on, setOn] = useState(false)
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Trabalho Final de IoT</Text>
+      {
+        on ? <>
+        <MaterialCommunityIcons name="lightbulb-on" size={250} color="black" onPress={() => setOn(false)} />
+        <Text>Pressione para desligar a lâmpada</Text>
+        </> : <>
+        <MaterialCommunityIcons name="lightbulb-on-outline" size={250} color="black" onPress={() => setOn(true)} />
+        <Text>Pressione para ligar a lâmpada</Text>
+        </>
+      }
     </View>
   );
 }
@@ -15,5 +26,10 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  title: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    marginBottom: 40
   },
 });
