fd985b438b67ef5c43dcd32371b7cfa7a5952abf
Bernardo Augusto Bastos Silva <bernardo.augusto.bastos@gmail.com>
2021-03-19 21:23:44 -0300

    connection to the server

--- App.tsx
+++ App.tsx
@@ -1,18 +1,55 @@
 import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { MaterialCommunityIcons } from '@expo/vector-icons'; 
+import axios from 'axios';
+
+const url = `https://io.adafruit.com/api/v2/jonatas_tonholo/feeds/tp-iot.status/data`;
+const headers = {
+  'X-AIO-Key': 'aio_FWjH4408o3lobc2r8SxgM3x4wMTr',
+}
 
 export default function App() {
-  const [on, setOn] = useState(false)
+  const [on, setOn] = useState('OFF')
+
+  const getValue = async (): Promise<void> => {
+    try {
+      const response = await axios.get<[{value: 'ON' | 'OFF'}]>(url, {
+          headers
+      });
+
+      console.log(response.data[0].value)
+      setOn(response.data[0].value);
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+  const setValue = async (value: 'ON' | 'OFF'): Promise<void> => {
+    try {
+      const response = await axios.post<{value: 'ON' | 'OFF'}>(url, {value},{
+        headers
+      });
+      
+      console.log(response.data.value)
+      setOn(response.data.value);
+    } catch (error) {
+        console.log(error)
+    }
+  }
+
+
+
+  getValue()
+
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Trabalho Final de IoT</Text>
       {
-        on ? <>
-        <MaterialCommunityIcons name="lightbulb-on" size={250} color="black" onPress={() => setOn(false)} />
+        on === 'ON' ? <>
+        <MaterialCommunityIcons name="lightbulb-on" size={250} color="black" onPress={() => setValue('OFF')} />
         <Text>Pressione para desligar a lâmpada</Text>
         </> : <>
-        <MaterialCommunityIcons name="lightbulb-on-outline" size={250} color="black" onPress={() => setOn(true)} />
+        <MaterialCommunityIcons name="lightbulb-on-outline" size={250} color="black" onPress={() => setValue('ON')} />
         <Text>Pressione para ligar a lâmpada</Text>
         </>
       }
